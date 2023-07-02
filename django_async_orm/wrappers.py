5a224b32d72bc85dc916b4e4f033bbad13d873a0
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-07-02 03:08:18 -0700

    remove useless wrapper funcs

--- django_async_orm/wrappers.py
+++ django_async_orm/wrappers.py
@@ -1,32 +1,13 @@
 from channels.db import database_sync_to_async as sync_to_async
 from django.contrib.auth import login, logout
-
-
-def _sync_render(*args, **kwargs):
-    from django.shortcuts import render
-
-    return render(*args, **kwargs)
-
-
-arender = sync_to_async(_sync_render, thread_sensitive=True)
-
-
-def _sync_login(*args, **kwargs):
-    return login(*args, *kwargs)
-
-
-alogin = sync_to_async(_sync_login, thread_sensitive=True)
-
-
-def _sync_logout(request):
-    return logout(request)
-
-
-alogout = sync_to_async(_sync_logout, thread_sensitive=True)
+from django.shortcuts import render
 
 
 def _sync_form_is_valid(form_instance):
     return form_instance.is_valid()
 
 
+arender = sync_to_async(render, thread_sensitive=True)
+alogin = sync_to_async(login, thread_sensitive=True)
+alogout = sync_to_async(logout, thread_sensitive=True)
 aform_is_valid = sync_to_async(_sync_form_is_valid, thread_sensitive=True)
