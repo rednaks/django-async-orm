038c6d6cb98972c606d119683cea5b50981acbba
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-07-02 03:03:36 -0700

    add wrappers.py

--- django_async_orm/wrappers.py
+++ django_async_orm/wrappers.py
@@ -8,25 +8,25 @@ def _sync_render(*args, **kwargs):
     return render(*args, **kwargs)
 
 
-async_render = sync_to_async(_sync_render, thread_sensitive=True)
+arender = sync_to_async(_sync_render, thread_sensitive=True)
 
 
 def _sync_login(*args, **kwargs):
     return login(*args, *kwargs)
 
 
-async_login = sync_to_async(_sync_login, thread_sensitive=True)
+alogin = sync_to_async(_sync_login, thread_sensitive=True)
 
 
 def _sync_logout(request):
     return logout(request)
 
 
-async_logout = sync_to_async(_sync_logout, thread_sensitive=True)
+alogout = sync_to_async(_sync_logout, thread_sensitive=True)
 
 
 def _sync_form_is_valid(form_instance):
     return form_instance.is_valid()
 
 
-async_form_is_valid = sync_to_async(_sync_form_is_valid, thread_sensitive=True)
+aform_is_valid = sync_to_async(_sync_form_is_valid, thread_sensitive=True)
